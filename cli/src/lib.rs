bf07007fc96639570dff7957654c5dcb0817505b
dtrlanz <68445623+dtrlanz@users.noreply.github.com>
2025-04-10 18:01:23 +0500

    Comment out mod declarations

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,5 +1,5 @@
-pub mod cli;
-pub mod commands;
+// pub mod cli;
+// pub mod commands;
 // pub mod config;   // Add later
 // pub mod error;    // Add later if needed
 // pub mod workspace; // Add later
